b3d79bb5bde50535c2d7af7ef6db288d5b1ab8f4
MEC43 <myung0403@gmail.com>
2024-07-08 18:08:01 +0900

    7

--- src/store/ref.js
+++ src/store/ref.js
@@ -1,2 +1,3 @@
-export const url = "https://localhost:8080";
+export const url = "https://port-0-fitweather-be-test-lycoakikfa27510a.sel5.cloudtype.app";
+// export const url = "https://localhost:8080";
 // export const url = "http://localhost:8080";
